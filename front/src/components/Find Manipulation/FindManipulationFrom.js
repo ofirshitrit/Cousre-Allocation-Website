7a17dc8b35d4450b06cab9db47cc4f0d45d9d4e0
OfirShitrit <ofirha18@gmail.com>
2024-11-10 14:24:20 +0200

    add CoursesAndStudents component to the other alogs

--- front/src/components/Find Manipulation/FindManipulationFrom.js
+++ front/src/components/Find Manipulation/FindManipulationFrom.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../Header";
+import CoursesAndStudents from "../CoursesAndStudents";
 
 export default function FindManipulationForm({ setSelectedAlgorithm }) {
   return (
@@ -9,6 +10,14 @@ export default function FindManipulationForm({ setSelectedAlgorithm }) {
         setSelectedAlgorithm={setSelectedAlgorithm}
         inHomePage={false}
       />
+      <div className="form-container">
+        <h2>Insert the parameters and run the algorithm <br /> Or <br /></h2>
+        <div className='random-container'>
+          <h2>Try with random example: </h2>
+         <button>Random</button>
+        </div>
+        <CoursesAndStudents />
+      </div>
     </>
   );
 }
