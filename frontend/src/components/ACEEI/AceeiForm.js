e706e077b3660dadca67414f2e4bb875e3d41a5e
OfirShitrit <ofirha18@gmail.com>
2024-11-13 11:39:59 +0200

    render results component when neccecary

--- frontend/src/components/ACEEI/AceeiForm.js
+++ frontend/src/components/ACEEI/AceeiForm.js
@@ -1,29 +1,28 @@
-import React, {useState} from 'react'
-import Header from '../Header';
-import '../../styles/forms.css'
-import CoursesAndStudents from '../CoursesAndStudents';
-import Results from '../Results';
+import React, { useState } from "react";
+import Header from "../Header";
+import "../../styles/forms.css";
+import CoursesAndStudents from "../CoursesAndStudents";
+import Results from "../Results";
 
-export default function AceeiForm({setSelectedAlgorithm}) {
-
+export default function AceeiForm({ setSelectedAlgorithm }) {
   const [results, setResults] = useState(null);
+  const [displayResults, setDisplayResults] = useState(false);
 
   const handleSubmit = async (e) => {
-
     console.log("in handleSubmit");
-    
-    e.preventDefault();  // Prevent default form submission behavior
-    
+
+    e.preventDefault(); // Prevent default form submission behavior
+
     // Gather form data
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData.entries());
 
     try {
       // Send data to Flask backend
-      const response = await fetch('http://127.0.0.1:5000/process', {
-        method: 'POST',
+      const response = await fetch("http://127.0.0.1:5000/process", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
       });
@@ -31,7 +30,8 @@ export default function AceeiForm({setSelectedAlgorithm}) {
       // Check response status and set results data
       if (response.ok) {
         const jsonResponse = await response.json();
-        setResults(jsonResponse.data);  // Store the returned data in state
+        setResults(jsonResponse.data); // Store the returned data in state
+        setDisplayResults(true)
       } else {
         console.error("Failed to submit form.");
       }
@@ -41,21 +41,33 @@ export default function AceeiForm({setSelectedAlgorithm}) {
   };
   return (
     <>
-      <Header headerContent= "ACEEI Algorithm" setSelectedAlgorithm={setSelectedAlgorithm} inHomePage={false}/>
-      <form onSubmit={handleSubmit} id="aceeiForm" className="form-container">
+      {displayResults === false && (
+        <>
+          <Header
+            headerContent="ACEEI Algorithm"
+            setSelectedAlgorithm={setSelectedAlgorithm}
+            inHomePage={false}
+          />
+          <form
+            onSubmit={handleSubmit}
+            id="aceeiForm"
+            className="form-container"
+          >
+            <h2>
+              Insert the parameters and run the algorithm <br /> Or <br />
+            </h2>
+            <div className="random-container">
+              <h2>Try with random example: </h2>
+              <button>Random</button>
+            </div>
+            <input type="submit" value="Run" />
 
-          <h2>Insert the parameters and run the algorithm <br /> Or <br /></h2>
-          <div className='random-container'>
-            <h2>Try with random example: </h2>
-          <button>Random</button>
-          </div>
-          <input type="submit" value="Run" />
-          {results && <Results data={results} />}
+            <CoursesAndStudents />
+          </form>
+        </>
+      )}
 
-          <CoursesAndStudents />
-      </form>
-
-
+      {displayResults === true && <Results data={results} />}
     </>
-  )
+  );
 }
