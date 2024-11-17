70483c90c420e33b816b2e30e6eb003c1d9853e1
OfirShitrit <ofirha18@gmail.com>
2024-11-17 13:42:48 +0200

    add conection between all the forms to the server

--- frontend/src/components/Find Manipulation/FindManipulationFrom.js
+++ frontend/src/components/Find Manipulation/FindManipulationFrom.js
@@ -1,24 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header";
 import CoursesAndStudents from "../CoursesAndStudents";
+import Results from "../Results";
+
 // TODO: add all the FindManipulationForm parameters
 // TODO: fix the GUI
 export default function FindManipulationForm({ setSelectedAlgorithm }) {
+  const [results, setResults] = useState(null);
+  const [displayResults, setDisplayResults] = useState(false);
+
+  const [numOfCourses, setNumOfCourses] = useState(0);
+  const [coursesCapacities, setCoursesCapacities] = useState({});
+
+  const [numOfStudents, setNumOfStudents] = useState(0);
+  const [budgets, setBudgets] = useState({});
+  const [coursesToTake, setCoursesToTake] = useState({});
+  const [ratings, setRatings] = useState({});
+
+  const handleSubmit = async (e) => {
+    console.log("in handleSubmit");
+
+    e.preventDefault();
+
+    const formData = {
+      numOfCourses,
+      coursesCapacities,
+      numOfStudents,
+      budgets,
+      coursesToTake,
+      ratings,
+    };
+
+    const data = Object.fromEntries(Object.entries(formData));
+
+    try {
+      // Send data to Flask backend
+      const response = await fetch("http://127.0.0.1:5000/process", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        setResults(jsonResponse.answer);
+        setDisplayResults(true);
+      } else {
+        console.error("Failed to submit form.");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
   return (
     <>
-      <Header
-        headerContent="Find Manipulation Algorithm"
-        setSelectedAlgorithm={setSelectedAlgorithm}
-        inHomePage={false}
-      />
-      <div className="form-container">
-        <h2>Insert the parameters and run the algorithm <br /> Or <br /></h2>
-        <div className='random-container'>
-          <h2>Try with random example: </h2>
-         <button>Random</button>
-        </div>
-        <CoursesAndStudents />
-      </div>
+      {displayResults === false && (
+        <>
+          <Header
+            headerContent="Find Manipulation Algorithm"
+            setSelectedAlgorithm={setSelectedAlgorithm}
+            inHomePage={false}
+          />
+          <form
+            onSubmit={handleSubmit}
+            id="findManipulationForm"
+            className="form-container"
+          >
+            <h2>
+              Insert the parameters and run the algorithm <br /> Or <br />
+            </h2>
+            <div className="random-container">
+              <h2>Try with random example: </h2>
+              <button>Random</button>
+            </div>
+            <input type="submit" value="Run" />
+
+            <CoursesAndStudents
+              numOfCourses={numOfCourses}
+              numOfStudents={numOfStudents}
+              setNumOfCourses={setNumOfCourses}
+              setCoursesCapacities={setCoursesCapacities}
+              setNumOfStudents={setNumOfStudents}
+              setBudgets={setBudgets}
+              setCoursesToTake={setCoursesToTake}
+              setRatings={setRatings}
+            />
+            
+          </form>
+        </>
+      )}
+
+      {displayResults === true && (
+        <Results
+          data={results}
+          setSelectedAlgorithm={setSelectedAlgorithm}
+          algoName={"Find Maipulation"}
+        />
+      )}
     </>
   );
 }
